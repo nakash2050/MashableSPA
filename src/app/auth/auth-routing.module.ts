7c7358eaad49f1588ae81df5e477f7da6dea6b43
Nakash Sheikh <nakashsheikh@Nakashs-MBP.attlocal.net>
2020-03-22 00:39:47 -0500

    Initial Commit

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    data: {
+      title: 'Authentication',
+      status: false
+    },
+    children: [
+      {
+        path: 'login',
+        loadChildren: () => import('./login/basic-login/basic-login.module').then(m => m.BasicLoginModule)
+      },
+      {
+        path: 'registration',
+        loadChildren: () => import('./registration/basic-reg/basic-reg.module').then(m => m.BasicRegModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
